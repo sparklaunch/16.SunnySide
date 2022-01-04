c90c32c2c9a4af1b4f9e7d67a4c2faf7167da1fb
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-01-04 17:41:55 +0900

    Create the writable and derived stores

---
+++ sunnyside-agency/src/Utils/Stores.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { writable, derived } from "svelte/store";
+    const width = writable(0);
+    export const isMobile = derived(width, ($width) => $width <= 1440);
+</script>
+
+<svelte:window bind:innerWidth={$width} />
